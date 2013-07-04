47ee1a1020b8816c11481efca237aa27c2d89465
John Smith <example@stanford.edu>
2013-07-04 21:47:36 +0000

    read and print html file

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/',function(request,response){
-    response.send("Hello World 2!");
+  fs.readFile('./index.html', function (err, data) {
+      if (err) throw err;
+         console.log(data.toString());
+  });
 });
 
 var port = process.env.PORT || 5000;
