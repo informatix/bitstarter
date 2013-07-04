0a9e020e30b9cdf3d80dd216bb8f8aa7c74285b2
John Smith <example@stanford.edu>
2013-07-04 22:00:28 +0000

    import bug fixed

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/',function(request,response){
   fs.readFile('./index.html', function (err, data) {
       if (err) throw err;
-         console.log(data.toString());
+      console.log(data.toString());
   });
 });
 
