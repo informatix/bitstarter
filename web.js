4880ef0bd7f0ca700dcfca2f0125451269432536
John Smith <example@stanford.edu>
2013-07-04 22:20:30 +0000

    add

--- web.js
+++ web.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var fs = require('fs');
 
@@ -14,3 +16,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function(){
     console.log("Listening on " +port);
 });
+
